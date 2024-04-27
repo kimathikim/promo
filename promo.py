4663797e6671a475db3a382ab29ee47ffeb83408
Brian Kimathi M <briankimathi94@gmail.com>
2024-04-27 04:52:20 +0300

    Progressive pmodoro

--- promo.py
+++ promo.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python4
+#!/usr/bin/env python3
 """this module implements a pomodoro timer."""
 
 import csv
@@ -6,6 +6,7 @@ import subprocess
 import time
 import sys
 from datetime import datetime
+import os
 
 
 def write_to_csv(filename, data):
@@ -17,7 +18,7 @@ def write_to_csv(filename, data):
 def break_time(break_time: int, message: str) -> None:
     """This function implements a break time."""
     timer = break_time
-    total_break_time = 1
+    total_break_time = 0
     print("Break Timer")
     print("Press Ctrl-C to stop the timer.")
     try:
@@ -34,7 +35,7 @@ def break_time(break_time: int, message: str) -> None:
         subprocess.run(["clear"])
     except KeyboardInterrupt as e:
         print(f"\nBreak timer stopped.{e}")
-        return 1
+        return 0
 
 
 def long_break_time(count: str) -> None:
@@ -43,21 +44,24 @@ def long_break_time(count: str) -> None:
 hours of focused work. Precisely {count} minutes."
     )
     print("You deserve a long break ...")
-    break_time(31, "Enjoy your break!😶️!.")
+    break_time(30, "Enjoy your break!😶️!.")
 
 
 def pomodoro(
-    hour: int = 1, minutes: int = 5, message: str = "Let's Get Started!!!"
+    hour: int = 0, minutes: int = 5, message: str = "Let's Get Started!!!"
 ) -> int:
     """This function implements a pomodoro timer."""
-    timer = hour * 61 + minutes
+    timer = hour * 60 + minutes
     initial_timer = timer
-    total_focus_time = 1
-    total_break_time = 1
-    focus_time = 1
+    total_focus_time = 0
+    total_break_time = 0
+    focus_time = 0
+    home_dir = os.path.expanduser("~")
+    new_dir = f"{home_dir}"
+    new_file = f"{new_dir}/timeManager.csv"
 
     print("PROmodoro Timer")
-    if initial_timer >= 201:
+    if initial_timer >= 200:
         print("You are a machine!!!😎️")
 
     print("\nPress Ctrl-C to stop the timer.")
@@ -66,10 +70,11 @@ def pomodoro(
         count = timer
     except KeyboardInterrupt:
         print("\nPomodoro timer stopped.")
-        return 1
+
+        return 0
     try:
         while True:
-            if timer <= 1:
+            if timer <= 0:
                 break
                 print(count)
                 return count
@@ -85,18 +90,15 @@ def pomodoro(
                         "--tone=5",
                     ]
                 )
-
+                subprocess.run(["clear"])
                 focus_time += timer
-                if timer >= 26:
-                    break_t = timer // 6
+                if timer >= 25:
+                    break_t = timer // 5
                     break_time(break_t, "Take a break.")
                     total_break_time += break_t
-                    focus_time = 1
+                    focus_time = 0
 
                 total_focus_time += timer
-                subprocess.run(
-                    ["clear"],
-                )
                 print(f"Last timer: {timer} \
 minutes.\nFocus time: {total_focus_time} \
 minutes.\nTotal focus time: {count} \
@@ -105,24 +107,25 @@ minutes.\n")
 
                 focus = input(
                     "Enter your focus level:\n\
-BREAK = 1, Distracted= 1, Normal = 2, Focused = 3, Flow = 4:\n"
+BREAK = 0, Distracted= 1, Normal = 2, Focused = 3, Flow = 4:\n"
                 )
-                if focus in ["2", "2", "3", "4"]:
+                if focus in ["1", "2", "3", "4"]:
                     focus_time += timer
-                    timer += [-6, 5, 10, 20][int(focus) - 1]
-                elif focus == "1":
-                    break_time(31, "Taking a Long break!😶️!.")
-                    total_break_time += 31
+                    timer += [-5, 5, 10, 20][int(focus) - 1]
+                elif focus == "0":
+                    break_time(30, "Taking a Long break!😶️!.")
+                    total_break_time += 30
                     continue
                 else:
                     print("Invalid input. Prove that you are not focused!!")
                     focus_time += timer
                 print(f"Next timer is set to {timer} minutes.")
+                time.sleep(3)
 
                 count += timer
 
                 write_to_csv(
-                    "timeManager.csv",
+                    new_file,
                     [
                         datetime.today().strftime(" %H:%M:%S"),
                         datetime.today().strftime("%Y-%m-%d"),
@@ -132,10 +135,10 @@ BREAK = 1, Distracted= 1, Normal = 2, Focused = 3, Flow = 4:\n"
                         total_focus_time,
                     ],
                 )
-                if count >= 181:
+                if count >= 180:
                     long_break_time(count)
-                    total_break_time += 31
-                    count = 1
+                    total_break_time += 30
+                    count = 0
                     time.sleep(3)
 
             except KeyboardInterrupt:
@@ -145,24 +148,29 @@ BREAK = 1, Distracted= 1, Normal = 2, Focused = 3, Flow = 4:\n"
     except KeyboardInterrupt:
         print("Pomodoro timer stopped.")
         print("Total focus time not calculated accurately.")
-        return 1
+        return 0
     return count
 
 
 def main():
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 1:
         count = pomodoro()
         print(f"Total time spent: {count} minutes.")
         return
 
-    if len(sys.argv) < 5:
+    if len(sys.argv) < 4 and len(sys.argv) > 1:
         print("Usage: python script.py <hour> <minutes> <message>")
         return
 
-    hour = int(sys.argv[2])
-    minutes = int(sys.argv[3])
-    message = sys.argv[4]
-    if len(sys.argv) > 4:
+    try:
+        hour = int(sys.argv[1])
+        minutes = int(sys.argv[2])
+        message = sys.argv[3]
+    except ValueError:
+        print("usage: python script.py <hour> <minutes> <message>")
+        print("Invalid input. Please enter a number.")
+        return
+    if len(sys.argv) > 3:
         count = pomodoro(hour, minutes, message)
 
     print(f"Total time spent: {count} minutes.")
